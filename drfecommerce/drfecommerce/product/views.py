2d98674f9912b0b3d5f51f90307f1281ac8d61ca
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-04-19 12:05:55 +0800

    Build: Implementing Brand

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
 
 class CategoryViewSet(viewsets.ViewSet):
@@ -16,4 +16,17 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
