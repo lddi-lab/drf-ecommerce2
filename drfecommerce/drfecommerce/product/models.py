ed2b14f00c0719bf6051e9c409048099259068a8
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-04-23 23:30:29 +0800

    Build: Creating Manager and QuerySet Methods

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+class ActiveQueryset(models.QuerySet):
+    def isactive(self):
+        return self.filter(is_active=True)
+
+
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=False)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    objects = ActiveQueryset.as_manager()
 
     class MPTTMeta:
         order_insertion_by = ["name"]
@@ -15,6 +22,8 @@ class Category(MPTTModel):
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=False)
+    objects = ActiveQueryset.as_manager()
 
     def __str__(self):
         return self.name
@@ -30,6 +39,8 @@ class Product(models.Model):
         "Category", on_delete=models.SET_NULL, null=True, blank=True
     )
     is_active = models.BooleanField(default=False)
+
+    objects = ActiveQueryset.as_manager()
 
     def __str__(self):
         return self.name
