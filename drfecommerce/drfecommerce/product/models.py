c3b482db178fd7633176570c0f7e9b9d62b92798
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-04-23 12:16:24 +0800

    Build: Handling Reverse Relationships in Serializers

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -39,5 +39,7 @@ class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
     stock_qty = models.IntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_line"
+    )
     is_active = models.BooleanField(default=False)
