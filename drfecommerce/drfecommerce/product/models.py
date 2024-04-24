69bd1b935745d2a2ba064a19e042b30ca87510eb
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-04-24 20:06:34 +0800

    Build: Admin Site Reverse Link Inline URLs

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -76,16 +76,16 @@ class ProductLine(models.Model):
 
 
 class ProductImage(models.Model):
-    name = models.CharField(max_length=100)
-    alternative_text = models.CharField(max_lengthh=100)
-    url = models.ImageField(upload_to=None)
-    product = models.ForeignKey(
+    alternative_text = models.CharField(max_length=100)
+    url = models.ImageField(upload_to=None, default="test.jpg")
+    productline = models.ForeignKey(
         ProductLine, on_delete=models.CASCADE, related_name="product_image"
     )
     order = OrderField(unique_for_field="productline", blank=True)
+    objects = ActiveQueryset.as_manager()
 
     def clean(self, exclude=None):
-        qs = ProductLine.objects.filter(productline=self.productline)
+        qs = ProductImage.objects.filter(productline=self.productline)
         for obj in qs:
             if self.id != obj.id and self.order == obj.order:
                 raise ValidationError("Duplicate value.")
@@ -95,4 +95,4 @@ class ProductImage(models.Model):
         return super(ProductImage, self).save(*args, **kwargs)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.url)
